63539401480af25c4ab3fc6e2b01313558f28c4a
Ivan <urbanovitchiv@yandex.ru>
2023-09-14 22:36:20 +0500

    use ament

--- include/ros_test/first_node.hpp
+++ include/ros_test/first_node.hpp
@@ -5,7 +5,6 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/int8.hpp"
 
 
 static const uint8_t c_n_sec = 5;
@@ -16,34 +15,35 @@ static const std::string c_first_node_msg = "Hello, I'm Alive!";
 
 class FirstNode : public rclcpp::Node
 {
-  public:
-    FirstNode()
-    : Node("first_node")
-    {
-      _ppublisher = this->create_publisher<std_msgs::msg::String>(c_topic_publisher, c_queue_size);
-      _psubscription = this->create_subscription<std_msgs::msg::String>(
-      c_topic_subscriber, c_queue_size, std::bind(&FirstNode::topic_callback, this, std::placeholders::_1));
-      _ptimer = this->create_wall_timer(
+public:
+  FirstNode()
+  : Node("first_node")
+  {
+    _ppublisher = this->create_publisher<std_msgs::msg::String>(c_topic_publisher, c_queue_size);
+    _psubscription = this->create_subscription<std_msgs::msg::String>(
+      c_topic_subscriber, c_queue_size,
+      std::bind(&FirstNode::topic_callback, this, std::placeholders::_1));
+    _ptimer = this->create_wall_timer(
       std::chrono::seconds(c_n_sec), std::bind(&FirstNode::timer_callback, this));
-    }
+  }
 
-  private:
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = c_first_node_msg;
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      _ppublisher->publish(message);
-    }
-    
-  private:
-    void topic_callback(const std_msgs::msg::String & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-    }
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = c_first_node_msg;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    _ppublisher->publish(message);
+  }
 
-  private:
-    rclcpp::TimerBase::SharedPtr _ptimer;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _ppublisher;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _psubscription;
+private:
+  void topic_callback(const std_msgs::msg::String & msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr _ptimer;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _ppublisher;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _psubscription;
 };
