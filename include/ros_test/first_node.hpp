1e2b4431b2b1793e4447aadb8acd9188afa42db4
Ivan <urbanovitchiv@yandex.ru>
2023-09-19 15:58:13 +0500

    fix code style

--- include/ros_test/first_node.hpp
+++ include/ros_test/first_node.hpp
@@ -7,10 +7,11 @@
 #include "std_msgs/msg/string.hpp"
 
 
+static const std::string c_node_name = "first_node";
 static const std::string c_topic_publisher = "publisher";
 static const std::string c_topic_subscriber = "subscription";
-static const uint8_t c_n_sec = 5;
-static const uint8_t c_queue_size = 10;
+static const uint8_t c_n_test_sec = 15;
+static const uint8_t c_queue_size_test = 20;
 static const std::string c_n_parameter = "timer_param";
 static const std::string c_queue_size_parameter = "queue_size_param";
 static const std::string c_first_node_msg = "Hello, I'm Alive!";
@@ -19,12 +20,12 @@ class FirstNode : public rclcpp::Node
 {
 public:
   FirstNode()
-  : Node("first_node")
+  : Node(c_node_name)
   {
-    this->declare_parameter(c_n_parameter, 15);
-    this->declare_parameter(c_queue_size_parameter, 20);
-    _n_sec = this->get_parameter(c_n_parameter).as_int();
-    _queue_size = this->get_parameter(c_queue_size_parameter).as_int();
+    this->declare_parameter(c_n_parameter, c_n_test_sec);
+    this->declare_parameter(c_queue_size_parameter, c_queue_size_test);
+    _n_sec = static_cast<uint8_t>(this->get_parameter(c_n_parameter).as_int());
+    _queue_size = static_cast<uint8_t>(this->get_parameter(c_queue_size_parameter).as_int());
     _ppublisher = this->create_publisher<std_msgs::msg::String>(c_topic_publisher, _queue_size);
     _psubscription = this->create_subscription<std_msgs::msg::String>(
       c_topic_subscriber, _queue_size,
--- include/ros_test/second_node.hpp
+++ include/ros_test/second_node.hpp
@@ -4,29 +4,30 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-
+static const std::string c_node_name = "second_node";
 static const std::string c_topic_publisher = "publisher";
 static const std::string c_topic_subscriber = "subscription";
-static const uint8_t c_queue_size = 10;
+static const uint8_t c_queue_size_test = 20;
 static const std::string c_queue_size_parameter = "queue_size_param";
 static const std::string c_second_node_msg = "Funny!";
 
 class SecondNode : public rclcpp::Node
 {
 public:
+
   SecondNode()
-  : Node("second_node")
+  : Node(c_node_name)
   {
-     this->declare_parameter(c_queue_size_parameter, 20);
-     _queue_size = this->get_parameter(c_queue_size_parameter).as_int();
+     this->declare_parameter(c_queue_size_parameter, c_queue_size_test);
+     _queue_size = static_cast<uint8_t>(this->get_parameter(c_queue_size_parameter).as_int());
     _ppublisher = this->create_publisher<std_msgs::msg::String>(c_topic_subscriber, _queue_size);
     _psubscription = this->create_subscription<std_msgs::msg::String>(
-      c_topic_publisher, c_queue_size,
+      c_topic_publisher, _queue_size,
       std::bind(&SecondNode::topic_callback, this, std::placeholders::_1));
   }
 
 private:
-  int8_t _queue_size;
+  uint8_t _queue_size;
 
 private:
   void topic_callback(const std_msgs::msg::String & msg) const
